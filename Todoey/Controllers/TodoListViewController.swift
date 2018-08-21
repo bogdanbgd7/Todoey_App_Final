3b6d52ad7a1b1993a892b6f2138247c89e97ceef
Bogdan Ponocko <bogdan-private@outlook.com>
2018-08-21 16:49:24 +0200

    Convert to MVC design pattern

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class TodoListViewController: UITableViewController{ //changed VC to TableVC
     
-    var itemArray = ["Find Mike", "Buy milk", "Buy gas", "Buy sneakers"]
+//    var itemArray = ["Find Mike", "Buy milk", "Buy gas", "Buy sneakers"]
     
-    var defaults = UserDefaults() 
+    var itemArray = [Item]()
+    var defaults = UserDefaults.standard //singleton
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        if let items = defaults.array(forKey: "DataStorage") as? [String] {
+        
+        let newItem = Item() //new item is new object of the type Item
+        newItem.title = "Find Mike"
+        newItem.done = true
+        itemArray.append(newItem)
+        
+        let newItem2 = Item() //new item is new object of the type Item
+        newItem2.title = "Buy Balenciaga"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item() //new item is new object of the type Item
+        newItem3.title = "Find AMG"
+        itemArray.append(newItem3)
+        
+        
+        
+        if let items = defaults.array(forKey: "DataStorage") as? [Item] { // as? [String] - we are doing this to be more specific
             itemArray = items
         }
         
@@ -28,6 +44,7 @@ class TodoListViewController: UITableViewController{ //changed VC to TableVC
     //MARK - TableView DataSource METHODS
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return itemArray.count
     }
     
@@ -35,7 +52,23 @@ class TodoListViewController: UITableViewController{ //changed VC to TableVC
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        
+        
+//              if item.done == true {
+//                    cell.accessoryType = .checkmark
+//               }
+//                else {                                    //we will use ternary operator instead IF
+//                    cell.accessoryType = .none
+//               }
+        
+        // value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        
         
         return cell
         
@@ -45,18 +78,9 @@ class TodoListViewController: UITableViewController{ //changed VC to TableVC
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-       // print(itemArray[indexPath.row])
+       itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-         //everytime i click on item, it displays checkmark
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else
-        {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true) //sminka, ne ostaje sivo kad se oznaci item, vec se vrati u belo
         
@@ -75,7 +99,11 @@ class TodoListViewController: UITableViewController{ //changed VC to TableVC
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in //akcija
             //what will happen once the user clicks the ADD item button on our alert
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            //self.itemArray.append(textField.text!) //dodaj tekst iz fieldText-a u niz
             
             self.defaults.set(self.itemArray, forKey: "DataStorage")
             
